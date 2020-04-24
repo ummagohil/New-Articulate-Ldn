9157bc49884874e1bc77b56cee6cfdd0b42eefb1
Umma Gohil <756D6D61@gmail.com>
2020-04-24 11:32:57 +0100

    Meta data change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import React from "react"
-
 import Background from "../images/ColourDesign.svg"
+import styled from "@emotion/styled"
+import Header from "../components/header"
+
+const Container = styled.div`
+  margin: 0;
+  height: 100%;
+  overflow: auto;
+`
 
 const IndexPage = () => (
-  <>
+  <Container>
     <Background />
-  </>
+  </Container>
 )
 
 export default IndexPage
