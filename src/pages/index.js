0b56a5b9c6ebc5586d4496cbe741573761b73108
Umma Gohil <756D6D61@gmail.com>
2020-04-24 12:04:27 +0100

    Temp fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,26 @@
 import React from "react"
 import Background from "../images/ColourDesign.svg"
 import styled from "@emotion/styled"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 const Container = styled.div`
   margin: 0;
-  height: 100%;
-  overflow: auto;
+  padding: 0;
+  overflow: hidden;
+  backround: #d3f4e1;
+`
+
+const StyledBackground = styled(Background)`
+  max-width: 100%;
+  position: fixed;
 `
 
 const IndexPage = () => (
-  <Container>
-    <Background />
-  </Container>
+  <Layout>
+    <Container>
+      <StyledBackground />
+    </Container>
+  </Layout>
 )
 
 export default IndexPage
